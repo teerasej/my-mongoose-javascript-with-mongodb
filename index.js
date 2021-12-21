e7a8959687edb20e12d1d009e20f41cdeaa3a53a
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-12-21 20:51:20 +0700

    Update - apply async main function - connect mongodb via mongoose - implement POST route to create new user with model - implement GET route to search user with email

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
 
 import express from 'express';
+import mongoose from 'mongoose';
+import UserModel from './schemas/user.js';
+
 
 const app = express();
 const port = 3000;
 
-// Route URL, เมนูในร้าน
-app.get('/', function(request, response){
-    response.send('hello');
-});
+app.use(express.json());
 
-// เริ่มการทำงาน
-app.listen(port, function(){
-    console.log('Server run at http://localhost:' + port);
-});
+main().catch(function(error){ console.log(error) });
+
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/dating_app');
+
+    // Route URL, เมนูในร้าน
+    app.get('/', function (request, response) {
+        response.send('hello');
+    });
+
+    app.post('/users', async function(request, response){
+        console.log(request.body);
+
+        // const newUser = new UserModel(request.body);
+        const newUser = new UserModel({
+            email: request.body.email,
+            password: request.body.password
+        });
+
+        const doc = await newUser.save()
+        
+        response.json(doc);
+    });
+
+    // /users/1234
+    // /users/training@nextflow.in.th
+    app.get('/users/:email', async function(request, response){
+        console.log(request.params.email);
+
+        const doc = await UserModel.findOne({ email: request.params.email });
+
+        if(doc) {
+            response.json(doc);
+        } else {
+            response.status(404).send('email not found')
+        }
+        
+    });
+
+
+
+    // เริ่มการทำงาน
+    app.listen(port, function () {
+        console.log('ตอนนี้เซิฟเวอร์ทำงานอยู่ที่ http://localhost:' + port);
+    });
+}
