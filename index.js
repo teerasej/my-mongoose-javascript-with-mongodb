18264a11e277b2ec7ed00c815be697e41889ffe1
Teerasej Jiraphatchandej <teerasej@gmail.com>
2021-12-21 22:44:39 +0700

    add handle error to create User

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 app.use(express.json());
 
-main().catch(function(error){ console.log(error) });
+main().catch(function (error) { console.log(error) });
 
 async function main() {
     await mongoose.connect('mongodb://localhost:27017/dating_app');
@@ -19,44 +19,49 @@ async function main() {
         response.send('hello');
     });
 
-    app.post('/users', async function(request, response){
-        console.log(request.body);
+    app.post('/users', async function (request, response) {
+        console.log(request.body)
 
-        // const newUser = new UserModel(request.body);
-        const newUser = new UserModel({
-            email: request.body.email,
-            password: request.body.password
-        });
+        // ปรับแต่่งมาใช้ try/catch เพื่อรองรับกรณีที่ mongoose คืนค่า error ออกมาจากการใช้งาน schema
+        let createdUser
+        try {
 
-        const doc = await newUser.save()
-        
-        response.json(doc);
+            const newUser = new UserModel(request.body)
+            createdUser = await newUser.save()
+
+        } catch (error) {
+            // ถ้า error ให้ใส่รหัส 500 และข้อความ error กลับไปที่ client
+            response.status(500).json(error.message)
+        }
+
+
+        response.status(200).json(createdUser)
     });
 
     // /users/1234
     // /users/training@nextflow.in.th
-    app.get('/users/:email', async function(request, response){
+    app.get('/users/:email', async function (request, response) {
         console.log(request.params.email);
 
         const doc = await UserModel.findOne({ email: request.params.email });
 
-        if(doc) {
+        if (doc) {
             response.json(doc);
         } else {
             response.status(404).send('email not found')
         }
-        
+
     });
 
     app.patch('/users', async (request, response) => {
         console.log(request.body)
-    
+
         // ใช้คำสั่ง updateOne เพื่ออัพเดตข้อมูล
         // สังเกตว่า parameter แรกคือ condition และ parameter ที่ 2 คือค่าที่จะส่งเข้าไปอัพเดต
         await UserModel.updateOne({ email: request.body.email }, request.body)
-    
+
         response.status(200).send('ok PATCH')
-    
+
 
         // ใช้ .findOneAndUpdate แทน ถ้าต้องการ doc กลับมาใช้งานจาก database ด้วย
         // const updatedDoc = await UserModel.findOneAndUpdate({ email: request.body.email }, request.body)
@@ -65,10 +70,10 @@ async function main() {
 
     app.delete('/users', async (request, response) => {
         console.log(request.body.id)
-    
+
         // เรามีคำสั่่ง .findByIdAndRemove ซึ่งทำให้ง่ายในการระบุ id เพื่อลบ document ออกจาก collection
         await UserModel.findByIdAndRemove(request.body.id)
-    
+
         response.status(200).send('ok DELETE')
     })
 
